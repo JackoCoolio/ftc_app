ab980195c3cb0262cb3d7fd37870ec468b5ff487
jacktwamb52 <jtwambolt@charter.net>
2018-01-08 20:10:07 -0600

    Added a new gyroscope library that in theory will work with the Adafruit IMU.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMan.java
@@ -27,11 +27,7 @@ public class OpMan extends OpMode {
       Sensors:
         -color
         -gyro?
-     */
-
-    //private DcMotor frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor;
-    //private DcMotor leftLiftMotor, rightLiftMotor, liftMotor;
-    //private Servo servo;
+    */
 
     private MotorGroup leftMotors;
     private MotorGroup rightMotors;
@@ -43,7 +39,6 @@ public class OpMan extends OpMode {
     private double winchPower;
 
     private boolean driveSlow;
-    private boolean liftSlow;
 
     // SENSORS //
     //private TouchSensor topTouch;
@@ -51,32 +46,16 @@ public class OpMan extends OpMode {
     // SENSORS //
 
     // CONSTANTS //
-    private final double liftPowerMult = 0.25d;
-    private final double baseLiftSpeed = 0.1d;
+//  private final double liftPowerMult = 0.25d;
+    private final double baseLiftSpeed = 0.35d;
     private final double drivePowerMult = .25d;
     private final double baseDriveSpeed = 0.95d;
     // CONSTANTS //
 
     @Override
     public void init() {
-//        frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left");
-//        rearLeftMotor = hardwareMap.get(DcMotor.class, "rear_left");
-//        frontRightMotor = hardwareMap.get(DcMotor.class, "front_right");
-//        rearRightMotor = hardwareMap.get(DcMotor.class, "rear_right");
-//
-//        leftLiftMotor = hardwareMap.get(DcMotor.class, "left_lift");
-//        rightLiftMotor = hardwareMap.get(DcMotor.class, "right_lift");
-//
+
         lift = hardwareMap.dcMotor.get("lift");
-
-        //servo = hardwareMap.get(Servo.class, "servo");
-
-        //topTouch = hardwareMap.touchSensor.get("top_touch");
-        //bottomTouch = hardwareMap.touchSensor.get("bottom_touch");
-
-//        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-//        rearLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-//        rightLiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         leftMotors = new MotorGroup(hardwareMap, "front_left", "rear_left");
         rightMotors = new MotorGroup(hardwareMap, "front_right", "rear_right");
@@ -90,11 +69,8 @@ public class OpMan extends OpMode {
     @Override
     public void loop() {
 
-        liftSlow = false;
         driveSlow = false;
 
-        if (gamepad2.right_bumper || gamepad2.left_bumper)
-            liftSlow = true;
         if (gamepad1.right_bumper || gamepad1.left_bumper)
             driveSlow = true;
 
@@ -117,7 +93,6 @@ public class OpMan extends OpMode {
             rightLiftPower = pwr;
         }
 
-//        double winchControls = gamepad2.right_trigger - gamepad2.left_trigger;
 //        if (topTouch.isPressed() && winchControls > 0) {
 //            winchPower = 0;
 //        } else if (bottomTouch.isPressed() && winchControls < 0) {
@@ -135,12 +110,6 @@ public class OpMan extends OpMode {
         if (driveSlow) {
             leftDrivePower *= drivePowerMult;
             rightDrivePower *= drivePowerMult;
-        }
-
-        if (liftSlow) {
-            leftLiftPower *= liftPowerMult;
-            rightLiftPower *= liftPowerMult;
-            winchPower *= liftPowerMult;
         }
 
         lift.setPower(winchPower);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/jewels/TestJewel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/jewels/TestJewel.java
@@ -2,15 +2,12 @@ package org.firstinspires.ftc.teamcode.autonomous.jewels;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * Created by jacktwamb52 on 1/4/2018.
- */
-
-@Autonomous(name = "Blue Jewel")
-public class BlueJewel extends OpMode {
+@Autonomous(name="ServoArm")
+public class TestJewel extends OpMode {
 
     private enum Color {
         Unknown,
@@ -18,19 +15,19 @@ public class BlueJewel extends OpMode {
         Blue
     }
 
-    private RedJewel.Color friendlyColor = RedJewel.Color.Blue;
+    protected Color friendlyColor = Color.Blue;
 
     // CONFIG FOR EASY TWEAKING //
     private final double defaultMiddle = 0.5;
     private final double pushDistance = 0.25;
     private final double servoSpeed = 0.001;
-    private final double downPosition = 0;
-    private final double upPosition = 0.5;
+    private final double pushSpeed = 0.001;
+    private final double downPosition = -1;
 
     private Servo lrServo;
     private Servo udServo;
 
-    private com.qualcomm.robotcore.hardware.ColorSensor sensor;
+    private ColorSensor sensor;
 
     private boolean pushed = false;
     private ElapsedTime timer = new ElapsedTime();
@@ -44,16 +41,16 @@ public class BlueJewel extends OpMode {
     }
 
     @Override
-    public void init_loop() {
-        udServo.setPosition(lerp(udServo.getPosition(), upPosition, servoSpeed));
-        lrServo.setPosition(lerp(lrServo.getPosition(), defaultMiddle, servoSpeed));
+    public void start() {
+
+        // Set servo positions.
+        lrServo.setPosition(0);
+        udServo.setPosition(downPosition);
+
     }
 
     @Override
     public void loop() {
-
-        telemetry.addData("UD Servo", udServo.getPosition());
-        telemetry.addData("LR Servo", lrServo.getPosition());
 
         // Lower vertical servo.
         udServo.setPosition(lerp(udServo.getPosition(), downPosition, servoSpeed));
@@ -67,11 +64,11 @@ public class BlueJewel extends OpMode {
 
 
         if (!pushed) {
-            RedJewel.Color foundColor = determineColor(sensor);
+            Color foundColor = determineColor(sensor);
             telemetry.addData("Color", foundColor.toString());
 
-            double push = 0;
-            if (foundColor.equals(RedJewel.Color.Unknown)) {
+            double push;
+            if (foundColor.equals(Color.Unknown)) {
                 push = defaultMiddle;
             } else if (foundColor.equals(friendlyColor)) {
                 push = defaultMiddle + pushDistance;
@@ -101,7 +98,7 @@ public class BlueJewel extends OpMode {
         lrServo = hardwareMap.servo.get("left_right");
         udServo = hardwareMap.servo.get("up_down");
 
-        sensor = hardwareMap.colorSensor.get("color");
+        sensor = hardwareMap.colorSensor.get("sensor");
 
     }
 
@@ -125,20 +122,16 @@ public class BlueJewel extends OpMode {
 
     }
 
-    private RedJewel.Color determineColor(com.qualcomm.robotcore.hardware.ColorSensor sensor) {
+    private Color determineColor(ColorSensor sensor) {
 
         if (sensor.red() > sensor.green() + sensor.blue()) {
-            return RedJewel.Color.Red;
+            return Color.Red;
         } else if (sensor.blue() > sensor.green() + sensor.red()) {
-            return RedJewel.Color.Blue;
+            return Color.Blue;
         } else {
-            return RedJewel.Color.Unknown;
+            return Color.Unknown;
         }
 
-    }
-
-    protected RedJewel.Color getFriendlyColor() {
-        return RedJewel.Color.Blue;
     }
 
 }
