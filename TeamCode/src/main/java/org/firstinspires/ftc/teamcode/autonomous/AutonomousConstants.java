9ec110de1e71b6814c1e2137022c865b92e47a20
jacktwamb52 <jtwambolt@charter.net>
2018-01-17 15:05:36 -0600

    Created AutonomousConstants class for easier constant changing.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousConstants.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+/**
+ * Created by jacktwamb52 on 1/17/2018.
+ */
+
+public class AutonomousConstants {
+    public static final double TURN_SPEED = .1d;
+    public static final double DRIVE_SPEED = .2d;
+    public static final double LIFT_SPEED = .95d;
+
+    public static final double DRIVE_OFF_TIME = 4d;
+    public static final double LIFT_RUNTIME = 4d;
+    public static final double DRIVE_BACK_TIME = 1.5d;
+
+    public static final double TOWARDS_SLOT_TIME_A = 2d;
+    public static final double TOWARDS_SLOT_TIME_B = 2d;
+    public static final double TOWARDS_SLOT_TIME_C = 2d;
+    public static final double TOWARDS_SLOT_TIME_D = 2d;
+}
