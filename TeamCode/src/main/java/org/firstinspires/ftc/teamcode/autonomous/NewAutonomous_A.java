e782c489a9620a252998c932a792b3ddb51d4d3e
jacktwamb52 <jtwambolt@charter.net>
2018-02-05 18:32:31 -0600

    Started work on new autonomous OpModes with DBD.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/NewAutonomous_A.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.autonomous.main.AutonomousConstants;
+import org.firstinspires.ftc.teamcode.autonomous.main.IMUAutonomous;
+import org.firstinspires.ftc.teamcode.autonomous.main.StagePresets;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.utility.MotorGroup;
+
+/**
+ * Created by jacktwamb52 on 2/5/2018.
+ */
+
+@Autonomous(name = "New Autonomous A")
+public class NewAutonomous_A extends IMUAutonomous {
+
+    Robot robot;
+
+    @Override
+    public Stage[] setStages() {
+
+        robot = new Robot(hardwareMap);
+        StagePresets.driveMotorGroups = new MotorGroup[] {robot.leftMotors, robot.rightMotors};
+
+        return new Stage[] {
+                MotorGroup.calibrateStage(
+                        telemetry,
+                        robot.leftMotors,
+                        robot.rightMotors
+                ),
+                StagePresets.drive(
+                        AutonomousConstants.DRIVE_SPEED,
+                        24,
+                        4,
+                        telemetry
+                ),
+                new Stage() { // Turn towards correct slot.
+
+                    double startHeading;
+                    double target;
+
+                    public void setup(double heading, ElapsedTime runtime) {
+                        robot.lift.setPower(0.95);
+
+                        target = vuMarkAngles.get("A").get(vuMark);
+
+                        startHeading = heading;
+                    }
+
+                    public boolean run(double heading, ElapsedTime runtime) {
+                        telemetry.addData("Start heading",startHeading);
+                        telemetry.addData("Heading",heading);
+                        telemetry.addData("Target angle",startHeading + target);
+                        telemetry.addData("Target offset",target);
+                        if (heading > startHeading + target) {
+                            robot.leftMotors.zero();
+                            robot.rightMotors.zero();
+                            return true;
+                        } else {
+                            robot.leftMotors.setPower(-AutonomousConstants.TURN_SPEED);
+                            robot.rightMotors.setPower(AutonomousConstants.TURN_SPEED);
+                            return false;
+                        }
+                    }
+                },
+                new Stage() {
+                    double dist;
+                    @Override public void setup(double heading, ElapsedTime runtime) {dist = vuMarkDistances.get("A").get(vuMark);}
+
+                    @Override
+                    public boolean run(double heading, ElapsedTime runtime) {
+                        return MotorGroup.runAndStopIfFinished(
+                                AutonomousConstants.DRIVE_SPEED,
+                                dist,
+                                3,
+                                telemetry,
+                                robot.leftMotors,
+                                robot.rightMotors
+                        );
+                    }
+                }
+        };
+    }
+}
