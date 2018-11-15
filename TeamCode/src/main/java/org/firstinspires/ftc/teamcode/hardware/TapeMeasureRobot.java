1573fa281faaba8f0209147f8dfc9a780a4586b1
jacktwamb52 <jtwambolt@charter.net>
2018-11-14 19:56:50 -0600

    Worked on base OpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TapeMeasureRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TapeMeasureRobot.java
@@ -2,13 +2,27 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.utility.MotorGroup;
+import org.firstinspires.ftc.teamcode.utility.Utility;
 
 public class TapeMeasureRobot extends Robot {
 
     MotorGroup leftDrive, rightDrive;
-    DcMotor arm;
+    DcMotor tapeMeasure, arm;
+
+    Servo grabber;
+
+    /*
+    Config
+     */
+    private final double servoSpeed = .001d;
+    private final double openPosition = 0d;
+    private final double closedPosition = 1d;
+    /*
+    Config
+     */
 
     public TapeMeasureRobot(HardwareMap hardwareMap) {
         super(hardwareMap);
@@ -20,16 +34,28 @@ public class TapeMeasureRobot extends Robot {
         leftDrive = new MotorGroup(hardwareMap, "left_front", "left_rear");
         rightDrive = new MotorGroup(hardwareMap, "right_front", "right_rear");
 
-        arm = hardwareMap.dcMotor.get("tape_measure");
+        tapeMeasure = hardwareMap.dcMotor.get("tape_measure");
+
+        arm = hardwareMap.dcMotor.get("arm");
+
+        grabber = hardwareMap.servo.get("grabber");
 
     }
 
     public void extend(double speed) {
-        arm.setPower(speed);
+        tapeMeasure.setPower(speed);
     }
 
     public void retract(double speed) {
-        arm.setPower(-speed);
+        tapeMeasure.setPower(-speed);
+    }
+
+    public void grab() {
+        grabber.setPosition(Utility.lerp(grabber.getPosition(), closedPosition, servoSpeed));
+    }
+
+    public void release() {
+        grabber.setPosition(Utility.lerp(grabber.getPosition(), openPosition, servoSpeed));
     }
 
 }
