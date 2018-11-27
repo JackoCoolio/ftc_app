e75609346b9b83fb046aacdb6513655d2443e378
jacktwamb52 <jtwambolt@charter.net>
2018-11-26 19:33:27 -0600

    Registered new modules in LaylaModular

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layla/LaylaModular.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layla/LaylaModular.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.layla;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.layla.modular.ArmModule;
 import org.firstinspires.ftc.teamcode.layla.modular.DriveModule;
 import org.firstinspires.ftc.teamcode.layla.modular.TMModule;
 import org.firstinspires.ftc.teamcode.modules.ModuleBasedOpMode;
 
+@TeleOp(name = "LaylaModular")
 public class LaylaModular extends ModuleBasedOpMode {
 
     @Override
     public void initModules() {
-        registerModules(DriveModule.class, TMModule.class);
+        registerModules(DriveModule.class, TMModule.class, ArmModule.class);
     }
 
 }
