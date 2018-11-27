aa751d47aa0e0230aadd0dc1531c25cf93701cca
jacktwamb52 <jtwambolt@charter.net>
2018-11-26 19:22:16 -0600

    Created TapeMeasureModule (TMModule)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layla/LaylaModular.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layla/LaylaModular.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.layla;
 
+import org.firstinspires.ftc.teamcode.layla.modular.DriveModule;
+import org.firstinspires.ftc.teamcode.layla.modular.TMModule;
 import org.firstinspires.ftc.teamcode.modules.ModuleBasedOpMode;
-import org.firstinspires.ftc.teamcode.modules.TestModule;
 
 public class LaylaModular extends ModuleBasedOpMode {
 
     @Override
     public void initModules() {
-        registerModules(TestModule.class);
+        registerModules(DriveModule.class, TMModule.class);
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layla/modular/TMModule.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.layla.modular;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.modules.Module;
+
+public class TMModule extends Module {
+
+    DcMotor tapeMeasure;
+
+    protected TMModule(HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry) {
+        super(hardwareMap, gamepad1, gamepad2, telemetry);
+    }
+
+    @Override
+    public void init() {
+        tapeMeasure = hardwareMap.dcMotor.get("tape_measure");
+    }
+
+    @Override
+    public void loop() {
+        if (-gamepad2.left_stick_y > .1) {
+            tapeMeasure.setPower(-gamepad2.left_stick_y);
+        } else if (-gamepad2.left_stick_y < .1) {
+            tapeMeasure.setPower(-gamepad2.left_stick_y);
+        } else {
+            tapeMeasure.setPower(0d);
+        }
+    }
+
+    @Override
+    public void telemetry() {
+        telemetry.addData("Tape Measure Power", -gamepad2.left_stick_y);
+    }
+}
