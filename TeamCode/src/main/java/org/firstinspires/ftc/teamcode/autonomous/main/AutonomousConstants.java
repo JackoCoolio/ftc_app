5a109ddecfe1bfb9453a9e54154e789a81da3bad
JackoCoolio <jtwambolt@charter.net>
2018-12-10 17:33:12 -0600

    Ebenezer's autonomous OpModes. Untested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/main/AutonomousConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/main/AutonomousConstants.java
@@ -7,16 +7,21 @@ package org.firstinspires.ftc.teamcode.autonomous.main;
 public class AutonomousConstants {
     public static final double TURN_SPEED = .25d;
     public static final double DRIVE_SPEED = .5d;
+    public static final double DESCENDING_TIME = 2.5d;
     public static final double LIFT_SPEED = .5d;
-
-    public static final double DRIVE_OFF_TIME_AC = 1.85d;
-    public static final double DRIVE_OFF_TIME_BD = 1.85d;
-    public static final double LIFT_RUNTIME = 3d;
+    public static final double TURN_FREE_TIME = 1d;
+    public static final double RETRACT_TIME = 2.5d;
     public static final double DRIVE_BACK_TIME = 0.5d;
-    public static final double PUSH_TIME =  0.3d;
 
-    public static final double TOWARDS_SLOT_TIME_A = .6d;
-    public static final double TOWARDS_SLOT_TIME_B = .6d;
-    public static final double TOWARDS_SLOT_TIME_C = .6d;
-    public static final double TOWARDS_SLOT_TIME_D = .6d;
+
+//    public static final double DRIVE_OFF_TIME_AC = 1.85d;
+//    public static final double DRIVE_OFF_TIME_BD = 1.85d;
+//    public static final double LIFT_RUNTIME = 3d;
+//
+//    public static final double PUSH_TIME =  0.3d;
+//
+//    public static final double TOWARDS_SLOT_TIME_A = .6d;
+//    public static final double TOWARDS_SLOT_TIME_B = .6d;
+//    public static final double TOWARDS_SLOT_TIME_C = .6d;
+//    public static final double TOWARDS_SLOT_TIME_D = .6d;
 }
