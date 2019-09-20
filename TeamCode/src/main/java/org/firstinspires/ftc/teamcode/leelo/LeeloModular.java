df3e642cb0824509b79f67394f27e148ed3da5cf
jacktwamb52 <jtwambolt@charter.net>
2019-09-19 19:48:13 -0500

    Started work on Leelo.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leelo/LeeloModular.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.leelo;
+
+import org.firstinspires.ftc.teamcode.modules.ModularOpMode;
+
+public class LeeloModular extends ModularOpMode {
+
+    @Override
+    protected void initModules() {
+        registerModules();
+    }
+}
