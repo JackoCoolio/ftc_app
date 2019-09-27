f6c6ae03eb0e816ee0c7203490aa721a53645525
jacktwamb52 <jtwambolt@charter.net>
2019-09-26 19:45:33 -0500

    Registered modules

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leelo/LeeloModular.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leelo/LeeloModular.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.leelo;
 
+import org.firstinspires.ftc.teamcode.leelo.modules.ArmModule;
+import org.firstinspires.ftc.teamcode.leelo.modules.DriveModule;
+import org.firstinspires.ftc.teamcode.leelo.modules.HandModule;
 import org.firstinspires.ftc.teamcode.modules.ModularOpMode;
 
 public class LeeloModular extends ModularOpMode {
 
     @Override
     protected void initModules() {
-        registerModules();
+        registerModules(HandModule.class, ArmModule.class, DriveModule.class);
     }
 }
